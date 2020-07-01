f2f6fcb93a74d776d518c9c4cf0c155180a904cd
unknown <singh_mail@yahoo.com>
2020-07-01 11:48:48 -0400

    Updates

---
+++ precache-manifest.5604a5624b4a10692fe7f4b9dd4d31fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67ee11e4f02d3b441f26b469039a1727",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "149d7eddedc893152646",
+    "url": "/monsters-rolodex/static/css/main.f018ca65.chunk.css"
+  },
+  {
+    "revision": "3755a802d800d78c2895",
+    "url": "/monsters-rolodex/static/js/2.76b028b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.76b028b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "149d7eddedc893152646",
+    "url": "/monsters-rolodex/static/js/main.18f447de.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
